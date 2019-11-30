b5d08360079fed01fb1854aecac22c063006b9f6
Trey Westrich <treyw05@gmail.com>
2019-11-30 16:23:06 -0600

    base layout done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { IndexContentComponent } from './index-content/index-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    SidebarComponent,
+    IndexContentComponent
   ],
   imports: [
     BrowserModule,
